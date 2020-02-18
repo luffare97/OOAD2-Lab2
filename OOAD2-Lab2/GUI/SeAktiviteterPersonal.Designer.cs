19ffde44b5a01aa2855e273a19cdc12d536b6803
luffare97 <daniel.agren@hotmail.se>
2020-02-18 20:05:16 +0100

    dethär

--- OOAD2-Lab2/GUI/SeAktiviteterPersonal.Designer.cs
+++ OOAD2-Lab2/GUI/SeAktiviteterPersonal.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(526, 419);
+            this.ClientSize = new System.Drawing.Size(574, 399);
             this.Name = "SeAktiviteterPersonal";
             this.Text = "Vald aktivitet";
             this.ResumeLayout(false);
